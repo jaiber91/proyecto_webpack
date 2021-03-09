172fa70bdf556923d549a0ad9ce408a18121a420
Jaiber Flor <jaiver.navia@gmail.com>
2021-03-09 10:19:10 -0500

    cambié el command por CI, en el archivo de netlify, por que me genera error al despelegarlo

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,4 +1,4 @@
-const API = 'https://randomuser.me/api/';
+const API = process.env.API; //El enlace de la API está en el archivo .env
 
 const getData = async (id) => {
   const apiURl = id ? `${API}${id}` : API;
